0bc4e0f4ef0c2901a45eaa49094bc45a81d58739
Jorge Jordan <j.jordanarenas@gmail.com>
2022-09-06 18:50:17 +0200

    Initial Commit

---
+++ UsersManagementApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsersManagementApp
+//
+//  Created by Jorge Jordán on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
